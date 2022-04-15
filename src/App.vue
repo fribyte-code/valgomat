6af99827c391b9c97bfbcdbe7ac6df55c61386e8
Mathias Haugsbø <mathiash98@gmail.com>
2022-04-15 23:21:43 +0200

    #3 Støtte for flere påstandslister
    Closes #3

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
 import { csvStatementsToJson } from "./csvStatementsToJson";
 // Use `?raw` to import file as text https://vitejs.dev/guide/assets.html#importing-asset-as-string
+import fiktiveStatements2 from "./data/2022-statements.json";
 import fiktiveStatementsCsv from "./data/fiktive-statements.csv?raw";
 import Forside from "./components/Forside.vue";
+import type { Statement } from "./types";
+import { computed } from "vue";
 
 const fiktiveStatements = csvStatementsToJson(fiktiveStatementsCsv);
+
+function getStatementFromUrlParam(): Statement[] {
+  const urlParams = new URLSearchParams(window.location.search);
+  const statementId = urlParams.get("pastander")?.toLowerCase();
+  console.debug(`Fetching statements based on url param ${statementId}`);
+  switch (statementId) {
+    case "2022lister":
+      // return csvStatementsToJson(lister2022);
+      console.debug("Showing statements for 2022-lister");
+      return fiktiveStatements2;
+    case "2022universitetsstyret":
+      // return csvStatementsToJson(universitetsstyret2022);
+      console.debug("Showing statements for universitetsstyret");
+      return fiktiveStatements2;
+    default:
+      return fiktiveStatements;
+  }
+}
+
+const statementsToServe = computed(() => {
+  return getStatementFromUrlParam();
+});
 </script>
 
 <template>
-  <Forside :statements="fiktiveStatements" />
+  <Forside :statements="statementsToServe" />
 </template>
 
 <style>
